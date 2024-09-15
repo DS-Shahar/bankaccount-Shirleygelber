2d1d0ca4c7f6ed50832db06e1d40ff067591d9fc
Shirleygelber <shir2gel@gmail.com>
2024-09-15 11:31:51 +0300

    BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -2,18 +2,15 @@ class BankAccount {
     private int balance;
     private int minBalance;
 
-    // Constructor for setting initial balance and min balance
     public BankAccount(int initialBalance, int minBalance) {
         this.balance = initialBalance;
         this.minBalance = minBalance;
     }
 
-    // Method to get the balance
     public int getBalance() {
         return balance;
     }
 
-    // Method to transfer money between accounts
     public void transfer(BankAccount otherAccount, int amount) {
         if (this.balance - amount >= minBalance) {
             this.balance -= amount;
